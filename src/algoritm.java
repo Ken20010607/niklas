cccf8356375029b3e643767a7995f91c76f35927
Ken20010607 <shenghao.he@elev.ga.ntig.se>
2019-09-24 12:12:42 +0200

    sumering klar

--- src/algoritm.java
+++ src/algoritm.java
@@ -4,34 +4,25 @@ public class algoritm {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int[] massaHeltal2 = { 0, 3, 5, 8 };
-		int summa = 0;
-		int tempTal;
-		int tempTal2;
-		int tempTal3;
 		
-		System.out.println("mata in ditt tal");
 		
-		tempTal=input.nextInt();
-		tempTal2=input.nextInt();
-		tempTal3=input.nextInt();
+		System.out.println("hur många tal ");
 		
-		for (int i : massaHeltal2)
-			System.out.println(i + tempTal + tempTal2 + tempTal3);
-			
+		  int antal = input.nextInt();
+	      int[] massaHeltal2 = new int[antal];
+		
+	      for (int i = 0; i < antal; i++) {
+	            System.out.println("ditt tal: ");
+	            massaHeltal2[i] = input.nextInt();
+	        }
 		
 
-		for (int i : massaHeltal2)
-			System.out.print(i+", ");
-		System.out.println();
-		for (int tal : massaHeltal2) {
-			System.out.println(summa + " + " + tal);
-			summa += tal;
-		}
+	      int summa = 0;
+	        for (int tal : massaHeltal2) {
+	            summa = summa + tal;
+	        }
 
-		System.out.println(summa);
+	        System.out.println("Summan är: " + summa);
+
+	    }
 	}
-	
-
-	
-}
